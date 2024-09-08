a1ee5c57029544e1f69a37f15441440bf3e7582c
Arif Mahmud Sisir <56744340+ArifMahmudSisir@users.noreply.github.com>
2024-09-08 10:29:46 +0600

    Update useAxiosSecure.jsx

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect } from "react";
 
 const axiosSecure = axios.create({
-  baseURL: "https://server-three-pied.vercel.app/api", 
+  baseURL: "server-green-nine.vercel.app/api", 
   // baseURL: "http://localhost:5000/api", 
 });
 
